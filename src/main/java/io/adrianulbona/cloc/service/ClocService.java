9a1062e453624d07c898d08789905bb170689da6
Adrian Bona <adrianulbona@gmail.com>
2016-10-31 22:09:07 +0200

    Add simplelogger

--- src/main/java/io/adrianulbona/cloc/service/ClocService.java
+++ src/main/java/io/adrianulbona/cloc/service/ClocService.java
@@ -5,6 +5,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import io.github.adrianulbona.cloc.CountryLocator;
 import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static ch.hsr.geohash.GeoHash.withCharacterPrecision;
+import static java.lang.String.format;
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 import static spark.Spark.*;
 
@@ -20,6 +23,8 @@ import static spark.Spark.*;
  * Created by adrianulbona on 08/08/16.
  */
 public class ClocService {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(ClocService.class);
 
 	private final Gson gson;
 	private final CountryLocator countryLocator;
@@ -29,11 +34,15 @@ public class ClocService {
 	}
 
 	public ClocService() throws IOException {
+		LOGGER.info("Initializing Service...");
 		this.gson = new Gson();
 		this.countryLocator = CountryLocator.fromFreshIndex();
+		LOGGER.info("Loaded Country Locator...");
+
 		get("/locate", this::locate, this.gson::toJson);
 		get("/locate/:lat/:long", this::locateLatLong, this.gson::toJson);
 		exception(JsonSyntaxException.class, (e, req, res) -> halt(SC_BAD_REQUEST, req.body()));
+		LOGGER.info("Created Routes...");
 	}
 
 	private List<Location> locate(Request req, Response res) {
@@ -48,8 +57,11 @@ public class ClocService {
 	}
 
 	private List<Location> locate(Coordinate coordinate) {
+		LOGGER.info(format("Searching locations for: %s", coordinate));
 		final GeoHash geoHash = withCharacterPrecision(coordinate.getLat(), coordinate.getLon(), 6);
-		return this.countryLocator.locate(geoHash.toBase32())
+		final List<String> locations = this.countryLocator.locate(geoHash.toBase32());
+		LOGGER.info(format("Found %d locations", locations.size()));
+		return locations
 				.stream()
 				.map(Location::new)
 				.collect(Collectors.toList());
